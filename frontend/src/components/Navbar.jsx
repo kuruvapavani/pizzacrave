208804eaa781a25013b4366463861f91fe672168
Kuruva Pavani <pavanikuruva2109@gmail.com>
2025-05-31 17:41:40 +0530

    create navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../assets/logo.png";
+import { faCartPlus, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const isLoggedIn = localStorage.getItem("userToken");
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar bg-white shadow-lg px-6 flex justify-between items-center w-full">
+      <div>
+        <Link to="/">
+          <img src={Logo} alt="Logo" className="md:h-20 h-16 w-auto cursor-pointer pt-2" />
+        </Link>
+      </div>
 
-export default Navbar
+      {/* Right Section */}
+      <div className="flex items-center space-x-6">
+        {/* Cart Icon */}
+        <div className="relative cursor-pointer">
+          <Link to="/my-cart" aria-label="Shopping Cart">
+            <FontAwesomeIcon
+              icon={faCartPlus}
+              className="text-hero hover:text-hero-dark text-xl transition-all duration-300 ease-in-out"
+            />
+          </Link>
+        </div>
+
+        {/* Conditional Rendering */}
+        {!isLoggedIn ? (
+          // Login Button if not logged in
+          <Link to="/login">
+            <button
+              className="bg-hero text-white md:px-6 md:py-2 px-4 py-1 text-sm md:text-base rounded-md 
+              hover:bg-opacity-90 transition-all duration-300 ease-in-out"
+              aria-label="Login"
+            >
+              Login
+            </button>
+          </Link>
+        ) : (
+          // Profile Icon if logged in
+          <Link to="/profile">
+            <FontAwesomeIcon
+              icon={faUser}
+              className="text-hero hover:text-hero-dark text-xl cursor-pointer transition-all duration-300 ease-in-out"
+              aria-label="Profile"
+            />
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'hero': '#FFA527',
+      },
+      screens: {
+        'sm': '480px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      fontFamily: {
+        itim: ['Itim', 'cursive'],
+      },
+    },
   },
   plugins: [],
-}
+};

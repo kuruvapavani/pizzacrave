50daab73501d7cce91f4d9f14fb05b70487aaf1f
Kuruva Pavani <pavanikuruva2109@gmail.com>
2025-07-18 21:08:58 +0530

    Fix case sensitivity issue in userModel import for deployment

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require("../models/UserModel");
+const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 const sendEmail = require("../utils/sendEmail");
